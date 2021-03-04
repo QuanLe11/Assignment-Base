6ac3b05c053c2e5cf4b0a1b09c36d58178411d12
QuanLe11 <quanle61500@gmail.com>
2021-03-04 01:05:18 -0500

    A1 Update: Got Search Results to Appear in a Box As An Unordered List

---
+++ public/script.js
@@ -0,0 +1,44 @@
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const zipcodes = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => zipcodes.push(...data))
+
+function findMatches(wordToMatch, zipcodes){
+    return zipcodes.filter(place => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.zip.match(regex) || place.city.match(regex)
+    });
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, zipcodes);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, "gi");
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const zipCode = place.zip.replace(regex, `<span class="hl">${this.value}</span>`);
+        const addressLine1 = place.address_line_1;
+        const restaurantName = place.name;
+        const inspectionResults = place.inspection_results;
+        const {category} = place;
+        return `
+            <li>
+                <span class="name">${restaurantName}</span>
+                <br>
+                <span class="address">${addressLine1}</span>
+                <br>
+                <span class="category">${category}</span>
+                <br>
+                <span class="inspection result">${inspectionResults}</span>
+            </li>
+            `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.input');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
