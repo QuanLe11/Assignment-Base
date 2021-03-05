1e0daa5780ecb63c721caf1b327a9dd8c9288737
QuanLe11 <quanle61500@gmail.com>
2021-03-05 16:17:00 -0500

    Finished A1: Implemented async function in JS code

--- public/script.js
+++ public/script.js
@@ -1,47 +1,54 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const zipcodes = [];
-const searchInput = document.querySelector('#input');
-const suggestions = document.querySelector('.suggestions');
+async function windowActions() {
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const searchInput = document.querySelector('#input');
+    const suggestions = document.querySelector('.suggestions');
+    
+    
+    // const data = fetch(endpoint);
+    // const results = data.json();
+    const request = await fetch(endpoint)
+        // .then(blob => blob.json())
+        // .then(data => zipcodes.push(...data))
 
-
-// const data = fetch(endpoint);
-// const results = data.json();
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => zipcodes.push(...data))
-
-function findMatches(wordToMatch, zipcodes){
-    return zipcodes.filter(place => {
-    const regex = new RegExp(wordToMatch, "gi");
-    return place.zip.match(regex) || place.city.match(regex)
+    const zipcodes = await request.json();
+    
+    function findMatches(wordToMatch, zipcodes) {
+        return zipcodes.filter(place => {
+        const regex = new RegExp(wordToMatch, "gi");
+        return place.zip.match(regex) || place.city.match(regex)
+        });
+    }
+    
+    function displayMatches(event) {
+        const matchArray = findMatches(event.target.value, zipcodes);
+        const html = matchArray.map(place => {
+            const regex = new RegExp(this.value, "gi");
+            const cityName = place.city;
+            const zipCode = place.zip;
+            const addressLine1 = place.address_line_1;
+            const addressLine2 = place.address_line_2;
+            const restaurantName = place.name;
+            const inspectionResults = place.inspection_results;
+            const {category} = place;
+            return `
+                <div class="box is-small">
+                    <li>
+                        <div class="name">${restaurantName}</div>
+                        <div class="category">${category}</div>
+                        <div class="address">${addressLine1}, ${addressLine2}</div>
+                        <div class="address">${cityName}, ${zipCode}</div>
+                        <div class="inspection result">${inspectionResults}</div>
+                    </li>
+                </div>
+                `;
+        }).join('');
+        suggestions.innerHTML = html;
+    }
+    
+    searchInput.addEventListener('input', displayMatches);
+    searchInput.addEventListener('keyup', (evt) => {
+        displayMatches(evt)
     });
 }
 
-function displayMatches(){
-    const matchArray = findMatches(this.value, zipcodes);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, "gi");
-        const cityName = place.city;
-        const zipCode = place.zip;
-        const addressLine1 = place.address_line_1;
-        const restaurantName = place.name;
-        const inspectionResults = place.inspection_results;
-        const {category} = place;
-        return `
-            <div class="box is-small">
-                <li>
-                    <!-- ADD ADDRESSES -->
-                    <div class="name">${restaurantName}</div>
-                    <div class="address">${addressLine1}</div>
-                    <div class="address">${cityName}, ${zipCode}</div>
-                    <div class="category">${category}</div>
-                    <div class="inspection result">${inspectionResults}</div>
-                </li>
-            </div>
-            `;
-    }).join('');
-    suggestions.innerHTML = html;
-}
-
-searchInput.addEventListener('input', displayMatches);
-// searchInput.addEventListener('keyup', displayMatches);
+window.onload = windowActions;
